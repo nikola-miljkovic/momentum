21b633bedb3ddd29cfa60c5e9a39c7c0ff2826ca
Nikola Miljkovic <milja1337@gmail.com>
2016-03-10 10:38:37 +0100

    Added homepage templates, with styles

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,13 +9,39 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="_index")
      */
     public function indexAction(Request $request)
-    {
+    {   
+        $num = rand();
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        return $this->render('index.html.twig', array(
+            'base_dir' => "Najjaci smo",
+            'moje_ime' => $num
         ));
     }
+     
+     /**
+      * @Route("/popular", name="_popular")
+      */
+     public function popularAction(Request $request)
+     {
+        return $this->render('index.html.twig');
+     }
+     
+     /**
+      * @Route("/active", name="_active")
+      */
+     public function activeAction(Request $request)
+     {
+        return $this->render('index.html.twig');
+     }
+     
+     /**
+      * @Route("/done", name="_done")
+      */
+     public function doneAction(Request $request)
+     {
+        return $this->render('index.html.twig');
+     }
 }
